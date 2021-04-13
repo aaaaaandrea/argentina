ce077fb002b98b008d8442eef43c962be0cb98ab
Andrea Židkova <zidkovaandrea66@gmail.com>
2021-04-13 19:34:03 +0200

    graf

--- js/script01.js
+++ js/script01.js
@@ -1,11 +1,47 @@
   $(function () {
 
-      $("#r").on("mouseenter", function () {
-        $(this).toggle();
-      })
+    $("#r").on("mouseenter", function () {
+      $(this).toggle();
+    })
 
-      $("h3").on("click", function () {
-          $(this).next().toggle();
-      });
+    $("h3").on("click", function () {
+      $(this).next().toggle();
+    });
 
   })
+
+  //grafy
+
+  //setup
+  const data = {
+    labels: [
+      'ostatní',
+      'Judaismus',
+      'Křesťanství'
+    ],
+    datasets: [{
+      label: 'Náboženství',
+      data: [5, 1, 94],
+      backgroundColor: [
+        'rgb(3, 139, 32)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  //config
+
+  const config = {
+    type: 'doughnut',
+    data: data,
+  };
+
+
+  var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+
+  //-------------------------------------
