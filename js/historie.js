309324d5b43e825188285db0a9ad9ec41a1d1d7e
Andrea Židkova <zidkovaandrea66@gmail.com>
2021-05-02 19:34:34 +0200

    historie

---
+++ js/historie.js
@@ -0,0 +1,28 @@
+//json
+const osobnosti = [{
+        "jmeno": "José de San Martín",
+        "narozeni": "25. února 1778",
+        "umrti": "17. srpna 1850",
+        "popisek": "Vůdců boje za nezávislost na Španělsku",
+        "obrazek": "img/José de San Martín.jpg",
+        "text": "Byl jedním z nejvýznamnějších latinskoamerických vůdců boje za nezávislost kontinentu na Španělsku. V Argentině je považován za národního hrdinu a v Peru za osvoboditele země."
+    }, {
+        "jmeno": "Facundo Arana",
+        "narozeni": "31. března 1972",
+        "umrti": "-",
+        "popisek": "Argentinský herec",
+        "obrazek": "-",
+        "text": "-"
+    },
+    {
+        "jmeno": "-",
+        "narozeni": "-",
+        "umrti": "-",
+        "popisek": "-",
+        "obrazek": "-",
+        "text": "-"
+    }
+];
+
+
+//jquery
